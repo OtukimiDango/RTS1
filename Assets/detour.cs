4da668e102c1a4c06a8727648ddc38bc3cbe7cc4
OtukimiDango <kawasimayuji@gmail.com>
2015-12-13 12:40:32 +0900

    初めてのコミット2015/12/13

---
+++ Assets/detour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class detour : MonoBehaviour {
+	public GameObject tgtPos;
+	public Vector3 myPos {
+		get;
+		set;
+	}
+	
+	private float moveTimer = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/summonsServant.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class summonsServant : MonoBehaviour {
+	public GameObject Soldier;
+
+	private int servantCount  = 0;
+
+	private bool summonSpace = true;
+
+	public static int sp = 50;
+
+	private GameObject spawnPoint;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spUp ());
+		spawnPoint = GameObject.Find("spawnPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Alpha1) && summonSpace && sp > 9){
+			StartCoroutine(summonServent(Soldier));
+		}
+	}
+	private IEnumerator spUp(){
+		while (true) {
+			yield return new WaitForSeconds(1);
+			sp++;
+		}
+	}
+	private IEnumerator summonServent(GameObject s){
+		sp -= 10;
+		summonSpace = false;
+		Object.Instantiate(s,spawnPoint.transform.position,spawnPoint.transform.rotation);
+		servantCount++;
+		yield return new WaitForSeconds(2.5f);
+		summonSpace = true;
+
+	}
+	void OnGUI() {
+		GUI.Label (new Rect (0, 0, 100, 30), "sp : "+sp);
+		GUI.Label (new Rect (100, 0, 100, 30), "Servants : " + servantCount);
+	}
+}
