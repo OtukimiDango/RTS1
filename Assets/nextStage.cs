11799e8d5b02be4bcc380459b5a2dcac76f638fc
smith.yuji <kawasimayuji@gmail.com>
2015-12-15 18:03:00 +0900

    startシーン追加

---
+++ Assets/nextStage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextStage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		Application.LoadLevel("stage1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void next(){
+		Application.LoadLevel("stage1");
+		Debug.Log ("yes");
+	}
+}
