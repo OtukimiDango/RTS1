727c65dc295f13aeff0de476182d907429480520
smith.yuji <kawasimayuji@gmail.com>
2015-12-18 21:38:47 +0900

    テクスチャ、クリスタルAseetの追加とTerrainの加工 2015/12/18

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private static GameObject Playercamera;
+	private static Vector3 cameraPos;
+	public static string mouseState;
+	public static GameObject saveChara;
+	public LayerMask mask;
+	// Use this for initialization
+	void Start () {
+		saveChara = GameObject.Find("BlueSoldier1");
+		//Debug.Log (saveChara.name);
+		mouseState = "normal";
+		Playercamera = GameObject.Find ("Camera");
+		cameraPos = Playercamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && mouseState == "normal") {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit,Mathf.Infinity,mask.value)){
+				clickCharacter (hit.collider.gameObject);
+			}
+		}
+		if (Input.GetKey ("d")) {
+			cameraPos.z += Time.deltaTime*50;
+			Playercamera.transform.position = cameraPos;
+		}
+		if (Input.GetKey ("a")) {
+			cameraPos.z -= Time.deltaTime*50;
+			Playercamera.transform.position = cameraPos;
+		}
+	}
+	private static void OnGUI(Vector3 position){
+		
+	}
+//	void OnMouseDrag()
+//	{
+//		Vector3 objectPointInScreen
+//		= Camera.main.WorldToScreenPoint(this.transform.position);
+//
+//		Vector3 mousePointInScreen
+//		= new Vector3(Input.mousePosition.x,
+//			Input.mousePosition.y,
+//			objectPointInScreen.z);
+//
+//		Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+//		mousePointInWorld.z = this.transform.position.z;
+//		this.transform.position = mousePointInWorld;
+//	}
+	private static void clickCharacter(GameObject clickChara){
+		if (clickChara.gameObject.tag == "Enemy" || clickChara.gameObject.tag == "StopEnemy" || clickChara.gameObject.tag == "Player" || clickChara.gameObject.tag == "StopPlayer") {
+			Debug.Log (clickChara.transform.parent.gameObject);
+			bool lineFlag = clickChara.transform.parent.gameObject.GetComponent<LineRenderer> ().enabled;
+			saveChara.GetComponent<LineRenderer> ().enabled = false;
+			saveChara.GetComponent<Light> ().enabled = false;
+			clickChara.transform.parent.gameObject.GetComponent<LineRenderer> ().enabled = !lineFlag;
+			clickChara.transform.parent.gameObject.GetComponent<Light> ().enabled = !lineFlag;
+			saveChara = clickChara.transform.parent.gameObject;
+		}
+	}
+
+}
