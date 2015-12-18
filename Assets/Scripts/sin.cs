037959820fafd63f7ecd066dd5b58c3724c86269
smith.yuji <kawasimayuji@gmail.com>
2015-12-18 23:48:34 +0900

    スタート画面をアニメーション化しレンズフレア追加。

---
+++ Assets/Scripts/sin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sin : MonoBehaviour {
+	private Vector3 mypos;
+	// Use this for initialization
+	void Start () {
+		mypos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(transform.name){
+		case ("1"):
+			mypos.x += Mathf.Sin (Time.time / 6)/2;
+			mypos.z += Mathf.Cos (Time.time / 6)/2;
+			transform.Rotate (0, 0.3f, 0);
+			transform.position = mypos;
+			break;
+		case("2"):
+			mypos.x += Mathf.Sin ((Time.time / 4)*-1);
+			mypos.z += Mathf.Cos ((Time.time / 4)*-1);
+			transform.Rotate (0, 0.5f, 0);
+			transform.position = mypos;
+			break;
+		case("3"):
+			transform.Rotate (0, 0.1f, 0);
+			break;
+	}
+}
+}
